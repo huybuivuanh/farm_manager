e1f965becc9a37ae95088b52fcdba486cce94965
seniorbaguette <102385296+seniorbaguette@users.noreply.github.com>
2023-10-15 12:58:02 -0600

    created dummy class to use for testing data manager

---
+++ src/main/java/org/InitialFarm/dummy.java
@@ -0,0 +1,36 @@
+package org.InitialFarm;
+
+public class dummy {
+
+    int dummyHeight ;
+    String dummyName;
+
+    public dummy( int dH1, String dN){
+        dummyHeight= dH1;
+        dummyName = dN;
+
+    }
+
+
+    public int getDummyHeight() {
+        return dummyHeight;
+    }
+
+    public void setDummyHeight(int h) {
+        dummyHeight = h ;
+    }
+
+
+    public String getDummyName() {
+        return dummyName;
+    }
+
+    public void setDummyName(String n) {
+        dummyName= n ;
+    }
+
+    public static void main(String[] args){
+        
+
+    }
+}
