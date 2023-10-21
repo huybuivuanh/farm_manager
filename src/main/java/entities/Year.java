c7d6261a66fffaa8dcbc1ef1d5a0da64983f6fdf
Uchechukwu, UDENZE <udenzeuchechukwu@gmail.com>
2023-10-21 02:23:22 -0600

    quality of life changes to Year.java

--- src/main/java/entities/Year.java
+++ src/main/java/entities/Year.java
@@ -83,9 +83,6 @@ public class Year{
         private Task getTask(){ return this.task; }
         private LocalDate getCompletedDate(){ return this.completedDate; }
     }
-    private static class Crop{}
-
-    private static class Chemical{}
 
 
     /**
