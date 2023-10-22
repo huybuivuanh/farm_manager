d790630e62bef7ef6bc48b27386591f44686d0cc
trb702 <trb702@mail.usask.ca>
2023-10-21 22:45:03 -0600

    starting basic ui for adding task

---
+++ src/main/java/org/openjfx/javafxmavenarchetypes/AddTaskView.java
@@ -0,0 +1,15 @@
+package org.openjfx.javafxmavenarchetypes;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Popup;
+
+public class AddTaskView {
+    Label albel = new Label("Popup");
+    TextField taskNameF = new TextField("Please up taskName");
+    TextField descriptionF = new TextField("Description");
+    TextField dueDate = new TextField("due date");
+    Button submitTask = new Button("submit");
+    Popup addTaskPop = new Popup();
+}
