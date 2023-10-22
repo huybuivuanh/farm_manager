b80d95a489bef8c6318eb9aa92aab560816b0a5c
trb702 <trb702@mail.usask.ca>
2023-10-21 22:44:54 -0600

    starting basic ui for adding task

--- src/main/java/org/openjfx/javafxmavenarchetypes/UITest.java
+++ src/main/java/org/openjfx/javafxmavenarchetypes/UITest.java
@@ -5,18 +5,19 @@ import javafx.application.Preloader;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import org.InitialFarm.Crop;
 import org.entities.*;
@@ -108,8 +109,27 @@ public class UITest extends Application {
         taskSelector.getChildren().addAll(btasks,bfield,bbins);
         taskSelector.setAlignment(Pos.CENTER);
 
-        RectButton addTask = new RectButton("#00ffff","add Task");
+
+        //Making the add pop up
+        Label albel = new Label("Popup");
+        TextField taskNameF = new TextField("Please up taskName");
+        TextField descriptionF = new TextField("Description");
+        TextField dueDate = new TextField("due date");
+        Button submitTask = new Button("submit");
+        Popup addTaskPop = new Popup();
+        addTaskPop.getContent().addAll(albel,taskNameF,descriptionF,dueDate,submitTask);
+
+        // Finished add pop up
+        RectButton addTask = new RectButton("","add Task");
+        addTask.setOnMouseClicked(e ->{
+            addTaskPop.show(stage);
+        });
         RectButton editTask = new RectButton("","edit task");
+
+        //This actually adds the functionality required.
+        editTask.setOnMouseClicked(e ->{
+            taskTable.getSelectionModel().getSelectedItem();
+        });
         RectButton markComplete = new RectButton("", "Mark Complete");
 
         RectButton viewCompleted = new RectButton("","newView");
