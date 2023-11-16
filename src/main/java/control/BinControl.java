ad79180a44b026e3031537203a6edd182e3b95b7
Huy Bui <wgv256@usask.ca>
2023-11-15 23:02:15 -0600

    Implemented BinController and added controller to BinView

--- src/main/java/control/BinControl.java
+++ src/main/java/control/BinControl.java
@@ -1,4 +1,95 @@
 package control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.InitialFarm.Crop;
+import org.InitialFarm.GrainBin;
+
+
 public class BinControl {
+    /**
+     * list of Bins
+     */
+    public ObservableList<GrainBin> binList;
+
+
+
+    /**
+     * constructor
+     */
+    public BinControl(){
+        binList = FXCollections.observableArrayList();
+    }
+
+    public void addBin(String bin_id, String bin_name, int bin_size, String bin_location, boolean hopper, boolean fan)
+    {
+        //check if Bin already exists
+        boolean binExists= false;
+
+
+        for (GrainBin bin : binList) {
+            if (bin.getID().equals(bin_id)) {
+                binExists = true;
+                break;
+            }
+        }
+        // if it doesn't, add it. If it does, report it.
+        if (!binExists){
+            GrainBin Bin = new GrainBin(bin_id, bin_name, bin_location, bin_size, hopper, fan);
+            binList.add(Bin);
+        }
+        else {
+            System.out.println("There already is a Bin with the desired ID");
+        }
+    }
+
+    public void deleteBin(String bin_id){
+        GrainBin deleted = null;
+        for (GrainBin bin : binList){
+            if (bin.getID().equals(bin_id)){
+                deleted = bin;
+                break;
+            }
+        }
+        if (deleted != null){
+            binList.remove(deleted);
+        }
+        else {
+            System.out.println("Bin with ID (" + bin_id + ") does not exist");
+        }
+    }
+
+    public void addCrop(String bin_id, Crop cropType, int grain, boolean inputBushels, boolean clean, boolean tough){
+        GrainBin binSearched = null;
+        for (GrainBin bin : binList){
+            if (bin.getID().equals(bin_id)){
+                binSearched = bin;
+                break;
+            }
+        }
+        if (binSearched != null){
+            binSearched.addCrop(cropType, grain, inputBushels, clean, tough);
+        }
+        else {
+            System.out.println("Can't find bin with ID (" + bin_id + ")");
+        }
+    }
+
+    public Crop getCrop(String bin_id){
+        for (GrainBin bin : binList){
+            if (bin_id.equals(bin.getID())){
+                return bin.getCurrentCrop();
+            }
+        }
+        return null;
+    }
+
+    public Crop getLastCrop(String bin_id){
+        for (GrainBin bin : binList){
+            if (bin_id.equals(bin.getID())){
+                return bin.getLastCrop();
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/org/openjfx/javafxmavenarchetypes/BinView.java
+++ src/main/java/org/openjfx/javafxmavenarchetypes/BinView.java
@@ -1,35 +1,40 @@
 package org.openjfx.javafxmavenarchetypes;
 
+import control.BinControl;
+import control.CropControl;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import org.InitialFarm.Crop;
 import org.InitialFarm.GrainBin;
+import org.bson.types.ObjectId;
 
 public class BinView extends StackPane implements ModelSubscriber {
 
     private VBox binPage = new VBox();
     private Scene MenuScene ;
+
+    private Scene binScene;
     private Stage stage;
 
     private TableView<GrainBin> binTable = new TableView<>();
-    private final ObservableList<GrainBin> grainBinData =
-            FXCollections.observableArrayList(
-                    new GrainBin("Tod", "the moon", 400, false, false),
-                    new GrainBin("Dave", "the moon", 4000, false, false),
-                    new GrainBin("The Big One", "the moon", 40000, false, true),
-                    new GrainBin("Nice", "the moon", 69, true, false)
-            );
+    BinControl binController = new BinControl();
+    private ObservableList<GrainBin> grainBinData = binController.binList;
+
+    TableView<Crop> cropTable = new TableView<>();
+    CropControl cropController = new CropControl();
+    ObservableList<Crop> cropData = cropController.currentCrop;
+
+    TableView<Crop> lastCropTable = new TableView<>();
+    ObservableList<Crop> lastCropData = cropController.lastCrop;
 
     public BinView(){
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,6 +51,11 @@ public class BinView extends StackPane implements ModelSubscriber {
         binlabel.setFont(new Font("Arial", 20));
 
         binTable.setEditable(true);
+
+        TableColumn<GrainBin, String> binIDCol = new TableColumn<GrainBin, String>("Bin ID");
+        binIDCol.setMinWidth(130);
+        binIDCol.setCellValueFactory(
+                new PropertyValueFactory<GrainBin, String>("ID"));
 
         TableColumn<GrainBin, String> binNameCol = new TableColumn<GrainBin, String>("Bin Name");
         binNameCol.setMinWidth(130);
@@ -72,24 +82,204 @@ public class BinView extends StackPane implements ModelSubscriber {
         binFanCol.setCellValueFactory(
                 new PropertyValueFactory<GrainBin, String>("fan"));
 
-        binTable.setItems(grainBinData);
-        binTable.getColumns().addAll(binNameCol,binLocationCol,binSizeCol,binHopperCol,binFanCol);
+        TableColumn<GrainBin, Integer> binBushelsCol = new TableColumn<GrainBin, Integer>("Bushels");
+        binBushelsCol.setMinWidth(70);
+        binBushelsCol.setCellValueFactory(
+                new PropertyValueFactory<GrainBin, Integer>("cropBushels"));
 
-        Button binsBackToMain = new Button("Back");
+        TableColumn<GrainBin, Integer> binCropLbsCol = new TableColumn<GrainBin, Integer>("Crop Lbs");
+        binCropLbsCol.setMinWidth(70);
+        binCropLbsCol.setCellValueFactory(
+                new PropertyValueFactory<GrainBin, Integer>("cropLbs"));
+
+        TableColumn<GrainBin, Boolean> binToughCol = new TableColumn<GrainBin, Boolean>("Tough");
+        binToughCol.setMinWidth(70);
+        binToughCol.setCellValueFactory(
+                new PropertyValueFactory<GrainBin, Boolean>("tough"));
+
+        TableColumn<GrainBin, Boolean> binCleanCol = new TableColumn<GrainBin, Boolean>("Clean");
+        binCleanCol.setMinWidth(70);
+        binCleanCol.setCellValueFactory(
+                new PropertyValueFactory<GrainBin, Boolean>("clean"));
+
+        binTable.setItems(grainBinData);
+        binTable.getColumns().addAll(binIDCol, binNameCol,binLocationCol,binSizeCol,binHopperCol,binFanCol, binBushelsCol, binCropLbsCol, binToughCol, binCleanCol);
+
+
+        VBox addBinBox = new VBox(30);
+        Scene addBinScene = new Scene(addBinBox,300,250);
+
+        TextField binIDInput = new TextField("Bin ID");
+        TextField binNameInput = new TextField("Bin Name");
+        TextField binSizeInput = new TextField("Bin Size");
+        TextField binLocation = new TextField("Bin Location");
+        CheckBox hopperInput = new CheckBox("Hopper?");
+        CheckBox fanInput = new CheckBox("Fan?");
+        Button submitBinInfo = new Button("Submit");
+
+
+        addBinBox.getChildren().addAll(binIDInput, binNameInput, binSizeInput, binLocation, hopperInput, fanInput, submitBinInfo);
+        Button addBin = new Button("Add Bin");
+        addBin.setOnMouseClicked(e ->{
+            stage.setScene(addBinScene);
+        });
+
+        submitBinInfo.setOnMouseClicked(e ->{
+            binController.addBin(binIDInput.getText(),binNameInput.getText(), Integer.parseInt(binSizeInput.getText()), binLocation.getText(), hopperInput.isSelected(), fanInput.isSelected() );
+            stage.setScene(binScene);
+        });
+
+        Button deleteBin = new Button("Delete Bin");
+        deleteBin.setOnAction(event -> {
+            binController.deleteBin(binTable.getSelectionModel().getSelectedItem().getID());
+            binTable.refresh();
+        });
+
+
+        VBox addCropBox = new VBox(30);
+        Scene addCropScene = new Scene(addCropBox,300,250);
+
+        TextField binID = new TextField("Bin ID");
+        TextField cropID = new TextField("Crop ID");
+        Label cropTypeLabel = new Label("Add New Crop Type");
+        TextField newCropTypeInput = new TextField();
+
+        ComboBox<String> cropVarietyInput = new ComboBox<>();
+        cropVarietyInput.getItems().addAll(cropController.cropVariety);
+
+        cropController.cropVariety.addListener((ListChangeListener<String>) change -> {
+            cropVarietyInput.setItems(cropController.cropVariety);
+        });
+
+        TextField bushelWeight = new TextField("Bushel Weight");
+        TextField grainInput = new TextField("Grain");
+        CheckBox inputBushels = new CheckBox("Measured in bushels?");
+        CheckBox cleanCrop = new CheckBox("Crop is clean?");
+        CheckBox toughCrop = new CheckBox("Crop is tough?");
+        Button submitCropInfo = new Button("Submit");
+
+        addCropBox.getChildren().addAll(binID, cropID, cropVarietyInput, cropTypeLabel, newCropTypeInput, bushelWeight, grainInput,
+                inputBushels, cleanCrop, toughCrop, submitCropInfo);
+        Button addCrop = new Button("Add Crop");
+        addCrop.setOnMouseClicked(e ->{
+            stage.setScene(addCropScene);
+        });
+
+        submitCropInfo.setOnMouseClicked(e ->{
+            Crop crop;
+            if (!newCropTypeInput.getText().isEmpty()){
+                cropController.addCropVariety(newCropTypeInput.getText());
+            }
+            if (cropVarietyInput.getValue() == null){
+                crop = new Crop(cropID.getText(), newCropTypeInput.getText(), newCropTypeInput.getText(), Float.parseFloat(bushelWeight.getText()));
+            }
+            else {
+                crop = new Crop(cropID.getText(), cropVarietyInput.getValue(), cropVarietyInput.getValue(), Float.parseFloat(bushelWeight.getText()));
+            }
+            binController.addCrop(binID.getText(), crop, Integer.parseInt(grainInput.getText()), inputBushels.isSelected(), cleanCrop.isSelected(), toughCrop.isSelected());
+            cropController.addCrop(crop);
+
+            // clear the form
+            cropVarietyInput.setValue(null);
+            newCropTypeInput.clear();
+
+            stage.setScene(binScene);
+        });
+
+        VBox binCropPage = new VBox(30);
+        Scene binCropScene = new Scene(binCropPage,300,250);
+
+
+
+        binTable.setOnMouseClicked(event -> {
+            GrainBin selectedData = binTable.getSelectionModel().getSelectedItem();
+            if (event.getClickCount() == 2) {
+                if (selectedData != null) {
+                    cropController.setCurrentCrop(selectedData.getCurrentCrop().getID());
+                    cropTable.setItems(cropData);
+                    if (selectedData.getLastCrop() != null) {
+                        cropController.setLastCrop(selectedData.getLastCrop().getID());
+                    }
+                    lastCropTable.setItems(lastCropData);
+                    stage.setScene(binCropScene);
+                }
+            }});
+
+
+
+        TableColumn<Crop, String> cropIDCol = new TableColumn<Crop, String>("Crop ID");
+        cropIDCol.setMinWidth(130);
+        cropIDCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, String>("ID"));
+
+        TableColumn<Crop, String> cropTypeCol = new TableColumn<Crop, String>("Crop Type");
+        cropTypeCol.setMinWidth(130);
+        cropTypeCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, String>("cropType"));
+
+        TableColumn<Crop, String> cropVarietyCol = new TableColumn<Crop, String>("Crop Variety");
+        cropVarietyCol.setMinWidth(130);
+        cropVarietyCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, String>("cropVariety"));
+
+        TableColumn<Crop, Float> bushelWeightCol = new TableColumn<Crop, Float>("Bushel Weight");
+        bushelWeightCol.setMinWidth(130);
+        bushelWeightCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, Float>("bushelWeight"));
+
+        cropTable.getColumns().addAll(cropIDCol, cropTypeCol, cropVarietyCol, bushelWeightCol);
+
+
+        TableColumn<Crop, String> cropIDCol2 = new TableColumn<Crop, String>("Crop ID");
+        cropIDCol.setMinWidth(130);
+        cropIDCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, String>("ID"));
+
+        TableColumn<Crop, String> cropTypeCol2 = new TableColumn<Crop, String>("Crop Type");
+        cropTypeCol.setMinWidth(130);
+        cropTypeCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, String>("cropType"));
+
+        TableColumn<Crop, String> cropVarietyCol2 = new TableColumn<Crop, String>("Crop Variety");
+        cropVarietyCol.setMinWidth(130);
+        cropVarietyCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, String>("cropVariety"));
+
+        TableColumn<Crop, Float> bushelWeightCol2 = new TableColumn<Crop, Float>("Bushel Weight");
+        bushelWeightCol.setMinWidth(130);
+        bushelWeightCol.setCellValueFactory(
+                new PropertyValueFactory<Crop, Float>("bushelWeight"));
+        lastCropTable.getColumns().addAll(cropIDCol2, cropTypeCol2, cropVarietyCol2, bushelWeightCol2);
+
+
+        Button cropToBin = new Button("Back To Bin");
+        cropToBin.setOnMouseClicked(e -> {
+            cropData.clear();
+            cropTable.refresh();
+            stage.setScene(binScene);
+        });
+        Label currentCropLabel = new Label("Current Crop");
+        Label lastCropLabel = new Label("Last Crop");
+        binCropPage.getChildren().addAll(cropToBin, currentCropLabel, cropTable, lastCropLabel, lastCropTable);
+
+        Button binsBackToMain = new Button("Back To Main");
         binsBackToMain.setOnMouseClicked(e ->{
             stage.setScene(MenuScene);
         });
 
-        VBox binvbox = new VBox();
-        binvbox.setSpacing(5);
-        binvbox.setPadding(new Insets(10, 0, 0, 10));
-        binPage.getChildren().addAll(binsBackToMain,binlabel, binTable);
+
+
+        HBox binFunctionBar = new HBox();
+        binFunctionBar.getChildren().addAll(addBin, deleteBin, addCrop, binsBackToMain);
+
+        binPage.getChildren().addAll(binFunctionBar,binlabel, binTable);
         this.getChildren().addAll(binPage);
     }
 
-    public void setStageMenu(Stage stage, Scene MenuScene){
+    public void setStageMenu(Stage stage, Scene MenuScene, Scene binScene){
         this.stage = stage;
         this.MenuScene = MenuScene;
+        this.binScene = binScene;
     }
 
     @Override
