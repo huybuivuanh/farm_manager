79b9e92a9d113a0877f398a4057ab1aeb1499cf6
Uchechukwu, UDENZE <udenzeuchechukwu@gmail.com>
2023-11-16 18:01:29 -0600

    added test files

--- src/main/java/TEST/TestAll.java
+++ src/main/java/TEST/TestAll.java
@@ -7,6 +7,10 @@ public class TestAll {
     public static void main(String[] args){
 
         TestField.main(args);
+        TestGrainBin.main(args);
+        TestCrop.main(args);
+        TestChemical.main(args);
+
 
     }
 
