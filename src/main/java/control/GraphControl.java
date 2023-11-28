bfa3c32a6ab704fbd8a30f95703d03951b121cd5
trb702 <trb702@mail.usask.ca>
2023-11-28 16:28:58 -0600

    fixed graph issue and added database functionality

--- src/main/java/control/GraphControl.java
+++ src/main/java/control/GraphControl.java
@@ -1,7 +1,32 @@
 package control;
 
+import javafx.collections.ObservableList;
+import org.InitialFarm.dataManager;
+import org.entities.Field;
+import org.entities.Year;
+
+import java.util.ArrayList;
+
 public class GraphControl {
 
+
+    ObservableList<Field> allFields;
+    dataManager dataManager = new dataManager();
+
     public GraphControl() {
+
+        allFields = dataManager.initializeFieldsFromDB();
+        System.out.println("graph control test");
+    }
+
+    public ObservableList<Field> getAllFields() {
+        return allFields;
+    }
+    public ArrayList<Year> getYears(Field field) {
+        ArrayList<Year> years = new ArrayList<>();
+        for (Year year : field.getYears()) {
+            years.add(year);
+        }
+        return years;
     }
 }
